0e2769dfc68ffdb5614a2e410d15ae2e11f987ce
hank_mj <987942918@qq.com>
2019-02-21 22:59:29 +0800

    feat: add the BFS algorithm

--- DFS.js
+++ DFS.js
@@ -12,13 +12,19 @@ const deepTraverse1 = (node, nodeList = []) => {
 const deepTraverse2 = node => {
   const stack = []
   const nodes = []
-  stack.push(node)
-  while (stack.length) {
-    const item = stack.pop()
-    const childern = item.childern
-    nodes.push(item)
-    for (let child of childern) {
-      stack.push(child)
+  if (node) {
+    stack.push(node)
+    while (stack.length) {
+      const item = stack.pop()
+      const childern = item.childern
+      nodes.push(item)
+        // node = [] stack = [parent]
+        // node = [parent] stack = [child3,child2,child1]
+        // node = [parent, child1] stack = [child3,child2,child1-2,child1-1]
+        // node = [parent, child1-1] stack = [child3,child2,child1-2]
+      for (let i = childern.length; i >= 0; i--) {
+        stack.push(childern[i])
+      }
     }
   }
   return nodes
