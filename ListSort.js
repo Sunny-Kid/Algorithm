27f5b014a1e997d197e2b65c17dbd2b0bcec4bcb
hank_mj <987942918@qq.com>
2019-03-24 15:11:51 +0800

    style: update the ListSourt JS code style

--- ListSort.js
+++ ListSort.js
@@ -5,40 +5,41 @@
  * 3. 父节点的值总是大于或等于任何一个子节点的值称为最大堆，父节点的值总是小于或等于任何一个子节点的值称为最小堆
  */
 
- // JS链表排序
-class Solution {            
-  findMiddle (head) {
-      var slow = head, fast = head.next;
-      while (fast != null && fast.next != null) {
-          fast = fast.next.next;
-          slow = slow.next;
-      }
-      return slow;
-  }    
+// JS链表排序
+class Solution {
+  findMiddle(head) {
+    var slow = head,
+      fast = head.next;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
 
   // 合并两个链表
-  merge (head1, head2) {
-      var dummy = new ListNode(0);
-      var tail = dummy;
+  merge(head1, head2) {
+    var dummy = new ListNode(0);
+    var tail = dummy;
 
-      while (head1 != null && head2 != null) {
-          if (head1.val < head2.val) {
-              tail.next = head1;
-              head1 = head1.next;
-          } else {
-              tail.next = head2;
-              head2 = head2.next;
-          }
-          tail = tail.next;
-      }
-
-      if (head1 != null) {
-          tail.next = head1;
+    while (head1 != null && head2 != null) {
+      if (head1.val < head2.val) {
+        tail.next = head1;
+        head1 = head1.next;
       } else {
-          tail.next = head2;
+        tail.next = head2;
+        head2 = head2.next;
       }
+      tail = tail.next;
+    }
 
-      return dummy.next;
+    if (head1 != null) {
+      tail.next = head1;
+    } else {
+      tail.next = head2;
+    }
+
+    return dummy.next;
   }
 
   /* 用二分的方式不断地递归细化链表，直到链表变成只有两个结点，
@@ -46,18 +47,18 @@ class Solution {
    * 值得说明的是每退出一层递归都有两个排好序的链表，
    * 每一层递归都要 merge 名为 left 和 right 的两个链表
    */
-  sortList (head) {
-      if (head == null || head.next == null) {
-          return head;
-      }
+  sortList(head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
 
-      mid = this.findMiddle(head);
+    mid = this.findMiddle(head);
 
-      right = this.sortList(mid.next);
-      // 保证 left 是 head 到 mid 的链表,不然 left 还是从 head 开始的整个链表
-      mid.next = null;
-      left = this.sortList(head);
+    right = this.sortList(mid.next);
+    // 保证 left 是 head 到 mid 的链表,不然 left 还是从 head 开始的整个链表
+    mid.next = null;
+    left = this.sortList(head);
 
-      return this.merge(left, right);
+    return this.merge(left, right);
   }
 }
