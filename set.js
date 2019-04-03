6fb9f798c6a3e2d4cc3fd6b51a199c5e532b3683
hank_mj <987942918@qq.com>
2019-04-03 12:55:28 +0800

    feat: enhance the Set

--- set.js
+++ set.js
@@ -30,4 +30,39 @@ class Set {
   get values () {
     return Object.keys(this.items)
   }
+
+  union (otherSet) {
+    const unionSet = new Set()
+    this.values.forEach(value => unionSet.add(value))
+    otherSet.values.forEach(value => unionSet.add(value))
+    return unionSet
+  }
+
+  intersection (otherSet) {
+    const intersectionSet = new Set()
+    this.values.forEach(value => {
+      if (otherSet.has(value)) {
+        intersectionSet.add(value)
+      }
+    })
+    return intersectionSet
+  }
+
+  difference (otherSet) {
+    const differenceSet = new Set()
+    this.values.forEach(value => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value)
+      }
+    })
+    return differenceSet
+  }
+
+  subset (otherSet) {
+    if (this.size > otherSet.size) {
+      return false
+    } else {
+      return this.values.every(value => otherSet.has(value))
+    }
+  }
 }
