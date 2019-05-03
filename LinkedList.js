47ebde01500b45b9bf33315efa7d8effa6b09684
任敏捷 <renminjie@bytedance.com>
2019-05-03 13:52:05 +0800

    feat: add the remove method in BST algorithm

--- LinkedList.js
+++ LinkedList.js
@@ -35,6 +35,7 @@ class LinkedList {
       let index = 0
       if (position === 0) {
         this.head = node
+        this.head.next = current
       } else {
         while (index++ < position) {
           previous = current
@@ -95,38 +96,5 @@ class LinkedList {
 
   size () {
     return this.length
-  }
-
-  inOrderTraverse(callback) {
-    const inOrderTraverseNode = (node, callback) => {
-      if (node !== null) {
-        inOrderTraverseNode(node.left, callback)
-        callback(node.key)
-        inOrderTraverseNode(node.right, callback)
-      }
-    }
-    inOrderTraverseNode(this.root, callback)
-  }
-
-  preOrderTraverse(callback) {
-    const preOrderTraverseNode = (node, callback) => {
-      if (node !== null) {
-        callback(node.key)
-        preOrderTraverseNode(node.left, callback)
-        preOrderTraverseNode(node.right, callback)
-      }
-    }
-    preOrderTraverseNode(this.root, callback)
-  }
-
-  postOrderTraverse(callback) {
-    const postOrderTraverseNode = (node, callback) => {
-      if (node !== null) {
-        postOrderTraverseNode(node.left, callback)
-        postOrderTraverseNode(node.right, callback)
-        callback(node.key)
-      }
-    }
-    postOrderTraverseNode(this.root, callback)
   }
 }
