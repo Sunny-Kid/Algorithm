05407b4ac0a6a0ab4cec3e533080a2e49287da34
任敏捷 <987942918@qq.com>
2019-05-19 15:29:23 +0800

    style: use 2 spaces

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -1,8 +1,8 @@
 function selectionSort(arr) {
 	let indexMin
-	for (let i = 0;i < arr.length;i++) {
+	for (let i = 0; i < arr.length; i++) {
 		indexMin = i
-		for (let j = i;j < arr.length;j++) {
+		for (let j = i; j < arr.length; j++) {
 			if (arr[indexMin] > arr[j]) {
 				indexMin = j
 			}
