d70cd5e48856ab1240af1257c42e07956e236cea
任敏捷 <987942918@qq.com>
2019-05-19 15:27:21 +0800

    fix: fix the insertionSort error

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -4,10 +4,11 @@ function insertionSort(arr) {
 	for (let i = 1;i < arr.length;i++) {
 		j = i
 		temp = arr[i]
-		while(j > 0 && arr[j-1] > arr[j]) {
+		while (j > 0 && arr[j-1] > temp) {
 			arr[j] = arr[j-1]
 			j--
 		}
+		// 到达索引位置0，插入最小值
 		arr[j] = temp
 	}
 }
