ff14363122b2455c5fe1fae390c2ed2c855ac97d
任敏捷 <987942918@qq.com>
2019-07-20 23:01:59 +0800

    feat: add the stock exchange

---
+++ LeetCode/121.买卖股票的最佳时机.js
@@ -0,0 +1,46 @@
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+
+注意你不能在买入股票前卖出股票。
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  if (!prices.length) return 0
+  let res = 0
+  let profit = []
+
+  for (let i = 0;i < prices.length;i++) {
+    profit[i] = []
+  }
+  // 0: 没有买入股票 1: 买入股票还没卖 2: 买入股票准备卖掉
+  profit[0][0] = 0
+  profit[0][1] = -prices[0]
+  profit[0][2] = 0
+
+  for (let i = 1;i < prices.length;i++) {
+    profit[i][0] = profit[i - 1][0]
+    profit[i][1] = Math.max(profit[i - 1][1], profit[i - 1][0] - prices[i])
+    profit[i][2] = profit[i - 1][1] + prices[i]
+    res = Math.max(res, profit[i][0], profit[i][1], profit[i][2])
+  }
+
+  return res
+};
