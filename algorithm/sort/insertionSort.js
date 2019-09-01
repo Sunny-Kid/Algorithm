e697eba87d1a71e1f89d6a64575eac017f5003b2
任敏捷 <987942918@qq.com>
2019-09-01 20:36:46 +0800

    feat: add the bucket sort

--- algorithm/sort/insertionSort.js
+++ algorithm/sort/insertionSort.js
@@ -5,16 +5,19 @@
  */
 function insertionSort(arr) {
 	let j
-	let temp
+	let key
 	for (let i = 1;i < arr.length;i++) {
-		j = i
-		temp = arr[i]
-		while (j > 0 && arr[j-1] > temp) {
-			arr[j] = arr[j-1]
+		j = i - 1
+		key = arr[i]
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j]
 			j--
 		}
-		// 到达索引位置0，插入最小值
-		arr[j] = temp
+		// 到达索引位置，插入最小值
+		arr[j + 1] = key
 	}
 	return arr
 }
+
+const arr = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+console.log(insertionSort(arr)) //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
