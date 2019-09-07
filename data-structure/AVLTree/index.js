e5fa2ff6a7b75eb7560adbc8930f2ca1db06221f
任敏捷 <987942918@qq.com>
2019-09-07 22:40:38 +0800

    feat: add AVL tree

---
+++ data-structure/AVLTree/index.js
@@ -0,0 +1,141 @@
+import BinarySearchTree from '../BinarySearchTree';
+import { COMPARE } from '../util';
+
+const BalanceFactor = {
+  UNBALANCED_RIGHT: 1,
+  SLIGHTLY_UNBALANCED_RIGHT: 2,
+  BALANCED: 3,
+  SLIGHTLY_UNBALANCED_LEFT: 4,
+  UNBALANCED_LEFT: 5
+};
+
+export default class AVLTree extends BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    super(compareFn);
+    this.compareFn = compareFn;
+    this.root = null;
+  }
+
+  getNodeHeight(node) {
+    if (node === null) {
+      return -1;
+    }
+    return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1;
+  }
+
+  getBalanceFactor(node) {
+    const heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+    switch (heightDifference) {
+      case -2:
+        return BalanceFactor.UNBALANCED_RIGHT;
+      case -1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+      case 1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+      case 2:
+        return BalanceFactor.UNBALANCED_LEFT;
+      default:
+        return BalanceFactor.BALANCED;
+    }
+  }
+
+  // 右旋转
+  rotationLL(node) {
+    const tmp = node.left;
+    node.left = tmp.right;
+    tmp.right = node;
+    return tmp;
+  }
+
+  // 左旋转
+  rotationRR(node) {
+    const tmp = node.right;
+    node.right = tmp.left;
+    tmp.left = node;
+    return tmp;
+  }
+
+  rotationLR(node) {
+    node.left = this.rotationRR(node.left);
+    return this.rotationLL(node);
+  }
+
+  rotationRL(node) {
+    node.right = this.rotationLL(node.right);
+    return this.rotationRR(node);
+  }
+
+  insert(key) {
+    this.root = this.insertNode(this.root, key);
+  }
+
+  insertNode(node, key) {
+    // insert node as in BST tree
+    if (node == null) {
+      return new Node(key);
+    } else if (this.compareFn(key, node.key) === COMPARE.LESS_THAN) {
+      node.left = this.insertNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === COMPARE.BIGGER_THAN) {
+      node.right = this.insertNode(node.right, key);
+    } else {
+      return node; // duplicated key
+    }
+    // balance the tree if needed
+    const balanceFactor = this.getBalanceFactor(node);
+    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      if (this.compareFn(key, node.left.key) === COMPARE.LESS_THAN) {
+        node = this.rotationLL(node);
+      } else {
+        return this.rotationLR(node);
+      }
+    }
+    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      if (
+          this.compareFn(key, node.right.key) === COMPARE.BIGGER_THAN
+      ) {
+        node = this.rotationRR(node);
+      } else {
+        return this.rotationRL(node);
+      }
+    }
+    return node;
+  }
+
+  removeNode(node, key) {
+    node = super.removeNode(node, key);
+    if (node == null) {
+      return node; // null, no need to balance
+    }
+    // verify if tree is balanced
+    const balanceFactor = this.getBalanceFactor(node);
+    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      const balanceFactorLeft = this.getBalanceFactor(node.left);
+      if (
+        balanceFactorLeft === BalanceFactor.BALANCED ||
+        balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT
+      ) {
+        return this.rotationLL(node);
+      }
+      if (
+          balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT
+          ) {
+        return this.rotationLR(node.left);
+      }
+    }
+    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      const balanceFactorRight = this.getBalanceFactor(node.right);
+      if (
+        balanceFactorRight === BalanceFactor.BALANCED ||
+        balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT
+      ) {
+        return this.rotationRR(node);
+      }
+      if (
+          balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT
+          ) {
+        return this.rotationRL(node.right);
+      }
+    }
+    return node;
+  }
+}
--- data-structure/util/index.js
+++ data-structure/util/index.js
@@ -1,9 +1,10 @@
 export const COMPARE = {
   LESS_THAN: -1,
+  EQUAL: 0,
   BIGGER_THAN: 1,
 }
 
 export function defaultCompare(a, b) {
-  if (a === b) return 0;
+  if (a === b) return COMPARE.EQUAL;
   return a < b ? COMPARE.LESS_THAN : COMPARE.BIGGER_THAN;
 }
