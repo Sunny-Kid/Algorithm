2d8f3187dc7506a60093b813629754aaeffbd296
任敏捷 <987942918@qq.com>
2019-09-10 00:15:07 +0800

    refactor: enhance the Dictionary

--- data-structure/Dictionary/index.js
+++ data-structure/Dictionary/index.js
@@ -1,30 +1,99 @@
+import { defaultToString } from '../util';
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+	}
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
 class Dictionary {
-	constructor () {
-		this.items = {}
+	constructor (toStrFn = defaultToString) {
+		this.toStrFn = toStrFn;
+		this.table = {};
 	}
 
-	set (key, value) {
-		this.items[key] = value
+	hasKey(key) {
+		return this.table[this.toStrFn(key)] != null;
 	}
 
-	get (key) {
-		return this.items[key]
+	set(key, value) {
+		if (key != null && value != null) {
+			const tableKey = this.toStrFn(key);
+			this.table[tableKey] = new ValuePair(key, value);
+			return true;
+		}
+		return false;
 	}
 
-	remove (key) {
-		delete this.items[key]
+	get(key) {
+		const valuePair = this.table[this.toStrFn(key)];
+		return valuePair == null ? undefined : valuePair.value; 
+	} 
+
+	remove(key) {
+		if (this.hasKey(key)) {
+			delete this.table[this.toStrFn(key)];
+			return true;
+		}
+		return false;
 	}
 
-	get keys () {
-		return Object.keys(this.items)
+	keyValues() { 
+		const valuePairs = []; 
+		for (const k in this.table) {
+			if (this.hasKey(k)) { 
+				valuePairs.push(this.table[k]);
+			} 
+		} 
+		return valuePairs; 
+	};
+
+	keys() {
+		return this.keyValues().map(valuePair => valuePair.key);
+	}
+	
+	values() {
+		return this.keyValues().map(valuePair => valuePair.value);
 	}
 
-	get values () {
-		return Object.keys(this.items).reduce((prev, curr) => {
-			prev.push(this.items[curr])
-			return prev
-		}, [])
+	size() {
+		return Object.keys(this.table).length;
 	}
+
+	isEmpty() {
+		return this.size() === 0;
+	}
+
+	clear() {
+		this.table = {};
+	}
+
+	forEach(callbackFn) {
+		const valuePairs = this.keyValues();
+		for (let i = 0; i < valuePairs.length; i++) {
+			const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
+			if (result === false) { 
+				break;
+			}
+		}
+	}
+
+	toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+    for (let i = 1; i < valuePairs.length; i++) {
+      objString = `${objString},${valuePairs[i].toString()}`;
+    }
+    return objString;
+  }
 }
   
 export default Dictionary
--- data-structure/util/index.js
+++ data-structure/util/index.js
@@ -8,3 +8,14 @@ export function defaultCompare(a, b) {
   if (a === b) return COMPARE.EQUAL;
   return a < b ? COMPARE.LESS_THAN : COMPARE.BIGGER_THAN;
 }
+
+export function defaultToString(item) {
+  if (item === null) {
+    return 'NULL';
+  } else if (item === undefined) {
+    return 'UNDEFINED';
+  } else if (typeof item === 'string' || item instanceof String) {
+    return `${item}`;
+  }
+  return item.toString();
+}
