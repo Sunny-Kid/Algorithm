beca74bcb9c2a924dd544ae6cd7fa93572f8231a
任敏捷 <987942918@qq.com>
2019-09-13 18:27:16 +0800

    feat: add the seperate chaining and linear probing

---
+++ data-structure/HashMap/linearProbing.js
@@ -0,0 +1,113 @@
+import { defaultToString } from '../util';
+
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+	}
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+export default class HashMap {
+  constructor(toStrFn = defaultToString) {
+    this.toStrFn = toStrFn;
+    this.table = {};
+  }
+
+  loseloseHashCode(key) {
+    if (typeof key === 'number') {
+      return key;
+    }
+    const tableKey = this.toStrFn(key);
+    let hash = 0;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash += tableKey.charCodeAt(i);
+    }
+    return hash % 37; 
+  }
+
+  djb2HashCode(key) {
+    const tableKey = this.toStrFn(key);
+    let hash = 5381;
+    for (let i = 0; i < tableKey.length; i++) {
+      hash = (hash * 33) + tableKey.charCodeAt(i);
+    }
+    return hash % 1013;
+  } 
+  
+  hashCode(key) {
+    return this.loseloseHashCode(key);
+  }
+
+  put(key, value) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
+      if(this.table[position] == null) {
+        this.table[position] = new ValuePair(key, pair);
+        return true;
+      } else {
+        let index = position + 1;
+        while (this.table[index] != null) {
+          index++;
+        }
+        this.table[index] = new ValuePair(key, pair);
+      }
+    }
+    return false;
+  }
+
+  get(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        return this.table[position].value;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        return this.table[position].value;
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const position = this.hashCode(key);
+    if (this.table[position] != null) { 
+      if (this.table[position].key === key) { 
+        delete this.table[position];
+        this.verifyRemoveSideEffect(key, position);
+        return true;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key ) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        delete this.table[index];
+        this.verifyRemoveSideEffect(key, index);
+        return true;
+      }
+    }
+    return false;
+  } 
+
+  verifyRemoveSideEffect(key, removedPosition) {
+    const hash = this.hashCode(key);
+    let index = removedPosition + 1;
+    while (this.table[index] != null) {
+      const posHash = this.hashCode(this.table[index].key);
+      if (posHash <= hash || posHash <= removedPosition) {
+        this.table[removedPosition] = this.table[index];
+        delete this.table[index];
+        removedPosition = index;
+      }
+      index++;
+    }
+  }
+}
