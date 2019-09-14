5892193d4e2ca44fdf7aec0c8a9e194c7a583710
任敏捷 <987942918@qq.com>
2019-09-14 22:13:43 +0800

    refactor: enhance the Set

--- src/data-structures/Set/index.js
+++ src/data-structures/Set/index.js
@@ -1,47 +1,51 @@
-class Set {
+export default class Set {
   constructor() {
     this.items = {};
   }
 
-  has(value) {
-    return this.items.hasOwnProperty(value);
-  }
-
-  add(value) {
-    if (!this.has(value)) {
-      this.items[value] = value;
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
       return true;
     }
     return false;
   }
 
-  remove(value) {
-    if (this.has(value)) {
-      delete this.items[value];
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
       return true;
     }
     return false;
   }
 
-  get size() {
-    return Object.keys(this.items).length;
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
   }
 
-  get values() {
-    return Object.keys(this.items);
+  values() {
+    return Object.values(this.items);
   }
 
   union(otherSet) {
     const unionSet = new Set();
-    this.values.forEach(value => unionSet.add(value));
-    otherSet.values.forEach(value => unionSet.add(value));
+    this.values().forEach(value => unionSet.add(value));
+    otherSet.values().forEach(value => unionSet.add(value));
     return unionSet;
   }
 
   intersection(otherSet) {
     const intersectionSet = new Set();
-    this.values.forEach(value => {
-      if (otherSet.has(value)) {
+    const values = this.values();
+    const otherValues = otherSet.values();
+    let biggerSet = values;
+    let smallerSet = otherValues;
+    if (otherValues.length - values.length > 0) {
+      biggerSet = otherValues;
+      smallerSet = values;
+    }
+    smallerSet.forEach(value => {
+      if (biggerSet.includes(value)) {
         intersectionSet.add(value);
       }
     });
@@ -50,7 +54,7 @@ class Set {
 
   difference(otherSet) {
     const differenceSet = new Set();
-    this.values.forEach(value => {
+    this.values().forEach(value => {
       if (!otherSet.has(value)) {
         differenceSet.add(value);
       }
@@ -58,11 +62,42 @@ class Set {
     return differenceSet;
   }
 
-  subset(otherSet) {
-    if (this.size > otherSet.size) {
+  isSubsetOf(otherSet) {
+    if (this.size() > otherSet.size()) {
       return false;
-    } else {
-      return this.values.every(value => otherSet.has(value));
     }
+    let isSubset = true;
+    this.values().every(value => {
+      if (!otherSet.has(value)) {
+        isSubset = false;
+        return false;
+      }
+      return true;
+    });
+    return isSubset;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    const values = this.values();
+    let objString = `${values[0]}`;
+    for (let i = 1; i < values.length; i++) {
+      objString = `${objString},${values[i].toString()}`;
+    }
+    return objString;
   }
 }
