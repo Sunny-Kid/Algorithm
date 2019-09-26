e34762d5d3e7288aa1060aa0843288acee7e8589
任敏捷 <987942918@qq.com>
2019-09-26 21:35:59 +0800

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['node_modules', 'dist']
+  testPathIgnorePatterns: ['node_modules', 'dist'],
+  collectCoverage: true,
+  coverageDirectory: './coverage'
 };
