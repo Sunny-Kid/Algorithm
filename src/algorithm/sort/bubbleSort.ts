4490ceae197759a27c8ed0bc7ea5577a795cb549
任敏捷 <987942918@qq.com>
2020-04-16 00:39:41 +0800

    refactor: add flag in bubble sort

--- src/algorithm/sort/bubbleSort.ts
+++ src/algorithm/sort/bubbleSort.ts
@@ -8,7 +8,7 @@ import { swap } from './utils';
 export default function bubbleSort(arr: number[]): number[] {
   if (arr.length <= 1) return;
   for (let i = 0; i < arr.length - 1; i++) {
-    let flag = false; // 表示有数据交换
+    let flag = false; // 表示有无数据交换
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
