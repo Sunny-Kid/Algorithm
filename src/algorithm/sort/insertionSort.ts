e9562f40c95f36160a2ab6e54e774e7d3633972e
任敏捷 <987942918@qq.com>
2020-04-16 01:30:23 +0800

    refactor: modify insertionSelect

--- src/algorithm/sort/insertionSort.ts
+++ src/algorithm/sort/insertionSort.ts
@@ -4,16 +4,20 @@
  * O(n^2)
  */
 export default function insertionSort(arr: number[]): number[] {
-  let key: number;
+  if (arr.length <= 1) return
   for (let i = 1; i < arr.length; i++) {
-    let j: number;
-    for (j = i; j >= 0; j--) {
-      if (arr[j] > key) {
-        arr[j] = arr[j - 1];
+    let j = i - 1;
+    const value = arr[i];
+    // 查找插入位置
+    for (; j >= 0; j--) {
+      if (arr[j] > value) {
+        arr[j + 1] = arr[j]; // 前几位数比后一位数大的话就做数据移动
+      } else {
+        break;
       }
     }
     // 到达索引位置，插入最小值
-    arr[j] = key;
+    arr[j + 1] = value;
   }
   return arr;
 }
