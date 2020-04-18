48c217d88694170900ad8fab184d9ce1461c6a45
任敏捷 <987942918@qq.com>
2020-04-18 13:26:31 +0800

    refactor: enhance binary search

--- src/algorithm/search/binarySearch.ts
+++ src/algorithm/search/binarySearch.ts
@@ -8,7 +8,12 @@ export default function binarySearch(item: number, arr: number[]): number {
   let low = 0;
   let high = arr.length - 1;
   while (low <= high) {
-    const mid = Math.floor((low + high) / 2);
+    /** 
+     * mid=(low+high)/2 这种写法是有问题的
+     * 因为如果 low 和 high 比较大的话，两者之和就有可能会溢出
+     * 改进的方法是将 mid 的计算方式写成 low+(high-low)/2
+     */
+    const mid = low + Math.floor((high - low) / 2);
     if (arr[mid] < item) {
       low = mid + 1;
     } else if (arr[mid] > item) {
